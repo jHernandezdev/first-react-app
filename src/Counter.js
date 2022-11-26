d4c8d37b2cc64fea4692f62d59b0e905c60595c5
Jose Hernandez Hernandez <jose.hernandez@wizeline.com>
2022-11-26 07:54:39 +0100

    First Commit

---
+++ src/Counter.js
@@ -0,0 +1,34 @@
+import {useState} from 'react';
+
+//Button increment
+function Button(props) {    
+    const handleClick = () => props.onClickFunction(props.increment);
+    return (        
+        <button onClick={handleClick}>
+            +{props.increment}
+        </button>
+    );
+}
+
+//Display
+function Display(props){
+    return (
+        <div>{props.message}</div>
+    );
+}
+
+function Counter() {
+    const [counter, setCounter] = useState(0);
+    const incrementCounter = (incrementValue) => setCounter(counter + incrementValue);
+    return (
+        <div>
+            <Button onClickFunction={incrementCounter} increment={1} />
+            <Button onClickFunction={incrementCounter} increment={5} />
+            <Button onClickFunction={incrementCounter} increment={10} />
+            <Button onClickFunction={incrementCounter} increment={100} />
+            <Display message={counter} />
+        </div>
+    )
+}
+
+export default Counter;
